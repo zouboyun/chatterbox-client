8bc746409c7b738973374005d986c68d95bd314b
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-28 12:10:52 -0700

    updated renderMessage function

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,7 +6,11 @@ var MessagesView = {
   },
 
   renderMessage: function(messageObj) {
-    var message = $('<p></p>').text(messageObj.text);
+    var message = $('<div></div>');
+    var username = $('<p></p>').text(messageObj.username);
+    var text = $('<p></p>').text(messageObj.text);
+    var room = $('<p></p>').text(messageObj.room);
+    message.append(username, text, room);
     MessagesView.$chats.append(message);
   }
 
